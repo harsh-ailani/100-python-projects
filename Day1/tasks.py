f69d7efd3dd5bcc83ee7b13904fa9397e50ca59f
harsh-ailani <ailani.harsh@gmail.com>
2025-07-12 04:08:22 +0530

    Day1 Commit

---
+++ Day1/tasks.py
@@ -0,0 +1,27 @@
+# Task1 - print a string
+print("Hello world!")
+
+# Task2 - print a multiple strings each on a new line
+print("Hello world!\nBrave new world!")
+
+# Task3 - Concatenate multiple strings
+print("Hello" + "Harsh")
+
+# Task4 - Concatenate multiple strings with a space
+print("Hello" + " " + "Harsh")
+
+# Task4 - Ask an input from the user and concatenate it with another string
+print("Hello " + input("What is your name?"))
+
+# Task5 - Ask an input from the user and concatenate it with multiple strings
+print("Hello " + input("What is your name?") + "!")
+
+# Task6 - Store values in variables
+name = "Jack"
+print(name)
+
+name = "Harsh"
+print(name)
+
+# Task7 - calculate the length of your name
+print("Length of you name is : " + str(len(input("What is your name?"))))
